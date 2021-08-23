738fef7039c9c156cc655c9f1bf5f452a80f0805
mrfunc <Mark-t.Kelly@analog.com>
2021-08-23 10:37:03 +0100

    ROT type class

--- cipher.hs
+++ cipher.hs
@@ -15,3 +15,12 @@ largestCharNumber = fromEnum(maxBound :: Char)
 rotChar :: Char -> Char
 rotChar charToEncrypt = rotN sizeOfAlphabet charToEncrypt
     where sizeOfAlphabet = 1 + fromEnum(maxBound :: Char)
+
+message :: [FourLetterAplphabet ]
+message = [L1, L3, L4, L1, L1, L2]
+
+encoder :: [FourLetterAplphabet] -> [FourLetterAplphabet]
+encoder vals = map rot4l vals
+        where
+            alphasize= 1 + fromEnum(maxBound :: FourLetterAplphabet)
+            rot4l = rotN alphasize
